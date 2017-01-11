c55a37d6468fbb46e30c227d50e18a5714433aa8
Kelly Honsinger <kellyhonsing@gmail.com>
2017-01-11 14:55:08 -0800

    changed title

--- src/jsx/frontPage/components/sponsorDiv.js
+++ src/jsx/frontPage/components/sponsorDiv.js
@@ -19,7 +19,7 @@ class SponsorDiv extends Component{
                     </a>
                 </div>
                 <div className="col-md-4 col-sm-6">
-                    <a href="portfolio-item.html">
+                    <a href="https://www.microsoft.com/en-us/">
                         <img className="img-responsive img-portfolio img-hover" src="https://mran.revolutionanalytics.com/assets/img/MSFT_logo_rgb_C-Gray.3b156229.png" alt=""></img>
                     </a>
                 </div>
