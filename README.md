9095d0c41375922a5804fdf61c02be8c08c976e1
root <root@phaseRacingUbuntu.localdomain>
2017-01-16 03:14:56 +0000

    modified server.js

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+##IMPORTANT
+App is running using ForeverJS
+To stop server from running execute command: forever stopall
+
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
 Below you will find some information on how to perform common tasks.<br>
--- server.js
+++ server.js
@@ -2,13 +2,29 @@ var express = require('express'),
 	app =  express(),
 	path = require('path');
 
-app.use(express.static('./build'));
+var helmet = require('helmet');
+var https = require('https');
+var fs = require('fs');
 
+var options = {
+	key: fs.readFileSync('server.key'),
+	cert: fs.readFileSync('server.crt')
+	};
+
+
+app.use(express.static('./build'));
+app.use(helmet());
 
 app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, './build', "/index.html"));
 });
 
+
+
+/*https.createServer(options, app).listen(3500, function(){
+	console.log(options.key);
+	console.log(options.cert);
+	});*/
 
 app.listen(3500, function(){
 console.log(__dirname);
