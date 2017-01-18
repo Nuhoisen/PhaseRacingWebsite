350e5411de6cb08e7b51d2e62e62949a8344b6e0
Kelly Honsinger <Kelly Honsinger>
2017-01-17 22:23:14 -0800

    added photos

--- src/jsx/frontPage/components/sponsorDiv.js
+++ src/jsx/frontPage/components/sponsorDiv.js
@@ -10,17 +10,17 @@ class SponsorDiv extends Component{
                 </div>
                 <div className="col-md-4 col-sm-6">
                     <a href="http://grided.epri.com/">
-                        <img className="img-responsive img-portfolio img-hover" src="http://grided.epri.com/Images/GridEd_930x300.jpg" alt=""></img>
+                        <img className="img-responsive img-portfolio img-hover" src="/images/sponsorPhotos/GridEd.jpg" alt=""></img>
                     </a>
                 </div>
                 <div className="col-md-4 col-sm-6">
                     <a href="http://www.altium.com/">
-                        <img className="img-responsive img-portfolio img-hover" src="http://www.altium.com/resources/images/media-release/altium_logo.jpg" alt=""></img>
+                        <img className="img-responsive img-portfolio img-hover" src="/images/sponsorPhotos/altium.jpg" alt=""></img>
                     </a>
                 </div>
                 <div className="col-md-4 col-sm-6">
                     <a href="https://www.microsoft.com/en-us/">
-                        <img className="img-responsive img-portfolio img-hover" src="https://mran.revolutionanalytics.com/assets/img/MSFT_logo_rgb_C-Gray.3b156229.png" alt=""></img>
+                        <img className="img-responsive img-portfolio img-hover" src="/images/sponsorPhotos/microsoft.png" alt=""></img>
                     </a>
                 </div>
                 
